b189a162c1c9000f8f8861f9e57f1465c8732475
Adam Tetelman <adam@techabt.com>
2016-11-07 23:20:42 -0800

    Improved upon the angular form simple project with styling, submittion, and validation.     add user friendly error message that keys off the inputs className     add submition button that is only enabled if ngForm.valid returns true     add custom styling     add review page/edit button that twiddles class submitted flag        flag is called via *ngIf and will cause dom updates     add new hero button that resets inputs and toggles flag on/off         toggled flag causes *ngIf to drop/reload the page 	    this is currently the only way to set an input to be pristine

--- angular-forms/app/hero-form.component.ts
+++ angular-forms/app/hero-form.component.ts
@@ -4,15 +4,27 @@ import { Hero }    from './hero';
 @Component({
   moduleId: module.id,
   selector: 'hero-form',
-  templateUrl: 'hero-form.component.html'
+  templateUrl: 'hero-form.component.html',
+  styleUrls: [ 'hero-form.component.css' ]
 })
 
 export class HeroFormComponent {
   powers = ['Really Smart', 'Super Flexible',
             'Super Hot', 'Weather Changer'];
-            
+
+  active = true;
+  submitted = false;
+
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
   submitted = false;
+  onSubmit() { this.submitted = true; }
+  
+  newHero() {
+    this.model = new Hero(42, '', '');
+    this.active = false;
+    setTimeout(() => this.active = true, 0);
+  }
+
   onSubmit() { this.submitted = true; }
 
   // TODO: Remove this when we're done
